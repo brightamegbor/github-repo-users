bbc455fe3277e7ce5c1e1129c0c9ef4b4507e37c
Bright Amegbor <brightcaiusamegbor@gmail.com>
2022-12-29 17:06:10 +0000

    updated the font family to Inter

--- app/src/main/java/com/example/githubrepousers/ui/theme/Type.kt
+++ app/src/main/java/com/example/githubrepousers/ui/theme/Type.kt
@@ -2,14 +2,26 @@ package com.example.githubrepousers.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.githubrepousers.R
+
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = Inter,
+
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
